f6374e6347ea645e3e22ccadfd741b3be50fdfc2
Galen Emanuel <galen.emanuel.fencing@gmail.com>
2020-04-01 23:40:42 -0400

    1.0

--- reactuipattern/src/App.js
+++ reactuipattern/src/App.js
@@ -7,8 +7,21 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
+      buttons: ["bulbasaur","charmander","squirtle","pikachu", "eevee"],
+      display: [],
       tabs: ["bulbasaur","charmander",'squirtle','pikachu','eevee']
     }
+    this.clickHandler = this.clickHandler.bind(this);
+  }
+  clickHandler(event){
+    let button = event.target;
+    let target = this.state.tabs.filter(tab => {
+      return (button.innerText===tab.props.title) 
+    })
+    this.setState({
+      display: target[0]
+    })
+
   }
   componentDidMount(){
     this.setState({
@@ -16,19 +29,23 @@ class App extends Component {
         return(
           <Tab key={i} title={tab}></Tab>
         )
+      }),
+      buttons: this.state.buttons.map((button,i)=> {
+        return(
+          <button key={i} onClick={this.clickHandler} name={button}>{button}</button>
+        )
       })
     })
   }
   render(){
     return (
       <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          
+        <header className="App-header"><h1>Pokemon!</h1>
+          <div className="buttonBox">
+            {this.state.buttons}
+          </div>
         </header>
-        <div className="tab-box">{this.state.tabs}</div>
+        <div className="tab-box">{this.state.display}</div>
       </div>
     );
   }
